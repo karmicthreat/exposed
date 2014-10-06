7b9b873da45f18024dc0fde614d8d958397b87ae
Richard Nienhuis <richardnienhuis@gmail.com>
2014-10-06 04:56:17 -0400

    Init commit

---
+++ pi/index.js
@@ -0,0 +1,25 @@
+  var socket = require('socket.io-client')('http://exposedgr.com/raspi');
+  var SerialPort = require("serialport").SerialPort
+  var serialPort = new SerialPort("/dev/teensy",{},false);
+
+  socket.on('connect', function(){
+    console.log("connected");
+    socket.on('event', function(data){console.log(data)});
+    socket.on('disconnect', function(){console.log("disconnected")});
+    socket.on('animation', function(data){console.log(data);
+      serialPort.open(function(error) {
+        if(error) {
+          console.log('failed' + error);
+        } else {
+          console.log('open');
+          serialPort.write("1",function(err,results) {
+            console.log('err' + err);
+            console.log('results' + results);
+          });
+        }
+      });      
+    });
+    socket.on('state_change', function(data){console.log(data)});
+    socket.on('power', function(data){console.log(data)});
+
+  });
